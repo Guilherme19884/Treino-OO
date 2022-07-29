bab2edc66ad94990fb6acc3ed6ee2be78fab2521
Guilherme Araújo <guilherme1984.araujo@gmail.com>
2022-07-28 23:40:18 -0300

    primeiro passo na OO

---
+++ js/script.js
@@ -0,0 +1,26 @@
+class Produto {
+  constructor() {
+    this.id = 0
+    this.nomeProduto=""
+    this.valor = 
+
+  }
+
+  adicionar() {
+    alert("Vamos adiconar um produto!");
+  }
+
+  listar() {
+    alert("Veja todos os itens!");
+  }
+
+  alterar() {
+    alert("Vamos atualizar!");
+  }
+
+  excluir() {
+    alert("Vamos excluir um produto!");
+  }
+}
+
+let produto = new Produto();
